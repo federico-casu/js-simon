c17e3f2ed716ecad46581db21c02c51e2c1fc5ac
federico-casu <federico.casu1101@gmail.com>
2024-01-18 21:07:19 +0100

    feat: divided countdown into hours, minutes and seconds

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,28 @@ tomorrow.setHours(9, 30, 0);
 
 const tomorrowMilliseconds = tomorrow.getTime();
 let countdownSeconds = (tomorrowMilliseconds - nowMilliseconds) / 1000;
+// let countdownSeconds = 10;
+let seconds = countdownSeconds % 60;
+let minutes = ((countdownSeconds - seconds) / 60) % 60;
+let hours = ((((countdownSeconds - seconds) / 60) - minutes) / 60);
 
 let clock;
 
 clock = setInterval(function (){
-    countdownHtml.innerHTML = `<h1>Mancano ${countdownSeconds} secondi alla correzione dell'esercizio</h1>`;
-    countdownSeconds--;
+    countdownHtml.innerHTML = `<h1>Mancano <br> ${hours}:${minutes}:${seconds} <br>alla correzione dell'esercizio</h1>`;
+    seconds--;
+    if (seconds < 0) {
+        if (minutes > 0) {
+            --minutes;
+            seconds = 59;
+        } else if (hours > 0) {
+            --hours;
+            minutes = 59;
+            seconds = 59;
+        } else {
+            clearInterval(clock);
+            countdownHtml.innerHTML = `<h1>E' ora di correggere l'esercizio!</h1>`;
+        }
+    }
 }, 1000);
 
-if (countdownSeconds === 0) {
-    clearInterval(clock);
-}
